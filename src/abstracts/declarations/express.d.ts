924aae208773098e9878cb0c66f3f89b993aeb39
MayorGit <amayowa@lachestry.com>
2022-10-20 18:30:52 +0300

    Fix after review
    [#INDEV22-255]

--- src/abstracts/declarations/express.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response, Request } from 'express';
-
-declare namespace Express {
-    export interface Response {
-        status?: (statusCode: number) => Response  // returning 'this' also didn't help here :(
-        redirect?: (page: string) => Response
-    }
-}
